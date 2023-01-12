33de3df683f1a51b71a68a783fcf09cb0a8429c8
AnnaKurashko <kuzann88@mail.ru>
2023-01-12 12:51:45 +0700

    добавила стилизацию добаления поста

--- src/Pages/AddingPostPage/AddingPostPage.jsx
+++ src/Pages/AddingPostPage/AddingPostPage.jsx
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import AddingPost from "../../Components/AddingPost/AddingPost";
 import { fetchAddPost } from "../../Storage/Slices/PostsSlile";
-
+import s from "./index.module.css"
 export default function AddingPostPage() {
 
     const dispatch = useDispatch();
@@ -13,7 +13,7 @@ export default function AddingPostPage() {
     return (
 
         <>
-            <h1>Добавления поста</h1>
+            <h1 className={s.title}>Добавления поста</h1>
 
             <AddingPost handleSubmit={handleSubmit} enabled={true} />
         </>
